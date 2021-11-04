a454eb7256bc98304cbde596df7c8250cc1e3c81
reneeb <info@perl-services.de>
2021-11-04 17:40:18 +0100

    add operation modules to check a session id and to delete it

---
+++ Kernel/GenericInterface/Operation/Session/SessionCheck.pm
@@ -0,0 +1,135 @@
+# --
+# Copyright (C) 2021 Perl-Services.de, https://perl-services.de
+# --
+# This software comes with ABSOLUTELY NO WARRANTY. For details, see
+# the enclosed file COPYING for license information (GPL). If you
+# did not receive this file, see https://www.gnu.org/licenses/gpl-3.0.txt.
+# --
+
+package Kernel::GenericInterface::Operation::Session::SessionCheck;
+
+use strict;
+use warnings;
+
+use Kernel::System::VariableCheck qw(IsStringWithData IsHashRefWithData);
+
+use parent qw(
+    Kernel::GenericInterface::Operation::Common
+    Kernel::GenericInterface::Operation::Session::Common
+);
+
+our $ObjectManagerDisabled = 1;
+
+=head1 NAME
+
+Kernel::GenericInterface::Operation::Session::SessionGet - GenericInterface Session Get Operation backend
+
+=head1 PUBLIC INTERFACE
+
+=head2 new()
+
+usually, you want to create an instance of this
+by using Kernel::GenericInterface::Operation->new();
+
+=cut
+
+sub new {
+    my ( $Type, %Param ) = @_;
+
+    my $Self = {};
+    bless( $Self, $Type );
+
+    # check needed objects
+    for my $Needed (
+        qw(DebuggerObject WebserviceID)
+        )
+    {
+        if ( !$Param{$Needed} ) {
+
+            return {
+                Success      => 0,
+                ErrorMessage => "Got no $Needed!"
+            };
+        }
+
+        $Self->{$Needed} = $Param{$Needed};
+    }
+
+    return $Self;
+}
+
+=head2 Run()
+
+Get session information.
+
+    my $Result = $OperationObject->Run(
+        Data => {
+            SessionID => '1234567890123456',
+        },
+    );
+    $Result = {
+        Success      => 1,                                # 0 or 1
+        ErrorMessage => '',                               # In case of an error
+        Data         => {
+            SessionValid => 1,
+        },
+    };
+
+=cut
+
+sub Run {
+    my ( $Self, %Param ) = @_;
+
+    if ( !IsHashRefWithData( $Param{Data} ) ) {
+
+        return $Self->ReturnError(
+            ErrorCode    => 'SessionGet.MissingParameter',
+            ErrorMessage => "SessionGet: The request is empty!",
+        );
+    }
+
+    if ( !$Param{Data}->{SessionID} ) {
+        return $Self->ReturnError(
+            ErrorCode    => 'SessionGet.MissingParameter',
+            ErrorMessage => "SessionGet: SessionID is missing!",
+        );
+    }
+
+    my $SessionObject = $Kernel::OM->Get('Kernel::System::AuthSession');
+
+    # Honor SessionCheckRemoteIP, SessionMaxIdleTime, etc.
+    my $Valid = $SessionObject->CheckSessionID(
+        SessionID => $Param{Data}->{SessionID},
+    );
+    if ( !$Valid ) {
+        return $Self->ReturnError(
+            ErrorCode    => 'SessionGet.SessionInvalid',
+            ErrorMessage => 'SessionGet: SessionID is Invalid!',
+        );
+    }
+
+    my $DateTimeObject = $Kernel::OM->Create('Kernel::System::DateTime');
+
+    $SessionObject->UpdateSessionID(
+        SessionID => $Param{Data}->{SessionID},
+        Key       => 'UserLastRequest',
+        Value     => $DateTimeObject->ToEpoch(),
+    );
+
+    return {
+        Success => 1,
+        Data    => {
+            SessionValid => 1,
+        },
+    };
+}
+
+1;
+
+=head1 TERMS AND CONDITIONS
+
+This software comes with ABSOLUTELY NO WARRANTY. For details, see
+the enclosed file COPYING for license information (GPL). If you
+did not receive this file, see L<https://www.gnu.org/licenses/gpl-3.0.txt>.
+
+=cut
